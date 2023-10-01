f7c44137fa6cdef104442c1265cf0d2c7e783142
MauricioSanchezA <89285001+MauricioSanchezA@users.noreply.github.com>
2023-09-30 20:13:33 -0500

    Update script.js
    new nav menu

--- script.js
+++ script.js
@@ -64,3 +64,20 @@ function seleccionar() {
 window.onscroll = function () {
   efectoHabilidades();
 };
+
+let text = document.querySelectorAll('ul li a').forEach((text) => {
+  text.innerHTML = text.innerText
+    .split('')
+    .map(
+      (letters, i) =>
+        `<span style="transition-delay:${i * 50}ms;">${letters}</span>`
+    )
+    .join('');
+});
+
+let cursor = document.querySelector('#cursor');
+document.addEventListener('mousemove', (e) => {
+  cursor.style.top = e.pageY + 'px';
+  cursor.style.left = e.pageX + 'px';
+});
+
